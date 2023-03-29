95f721eaf2fb779b603f740ffc2edb2f510b5006
NikitaSizPl <121577151+NikitaSizPl@users.noreply.github.com>
2023-03-29 11:19:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    token: str = '6008230821:AAHZlGJoWgA6mGQ8_euHIsisDOUEMC-9nPU'
+    token: str = ''
     pay_token: str = ''
     admin: int = None
 
