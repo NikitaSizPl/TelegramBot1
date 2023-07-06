2cb6fad663f702311068bbb52cb6fdf648449c75
nikita <nikitasizpl@gmail.com>
2023-07-06 10:44:32 +0200

    Upadte

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -6,3 +6,7 @@ from keyboards import callback_ukr
 from keyboards import callback_pl
 from keyboards import callback_eng
 from keyboards import zakaz
+from keyboards import menu_rus
+from keyboards import menu_ukr
+from keyboards import menu_pl
+from keyboards import menu_eng
