9f68f3841048ea1c35f5061ea68a3356ddd677da
nikita <nikitasizpl@gmail.com>
2023-12-25 13:21:17 +0100

    Upadte2

--- handlers/other.py
+++ handlers/other.py
@@ -1,13 +1,11 @@
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
-from create_bot import bot
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from create_bot import bot
 
 storage = MemoryStorage()
 
-global insta, deleviri, info, chat_id_nikita
 
 # Описываем состояния
 class OrderState(StatesGroup):
@@ -17,9 +15,7 @@ class OrderState(StatesGroup):
     CONFIRMATION = State()
 
 
-
 # Обрабатываем команду /order
-
 async def new_order(callback_query: types.CallbackQuery):
     await callback_query.message.answer('Напиши пожалуйста Ваш Instagram')
     await OrderState.INSTA.set()
@@ -27,60 +23,46 @@ async def new_order(callback_query: types.CallbackQuery):
 
 async def order_insta(message: types.Message, state: FSMContext):
     # Получаем имя пользователя, сохраняем его и переходим к следующему состоянию
-    insta = message.text
-    await state.update_data(insta=insta)
+    await state.update_data(insta=message.text)
     await OrderState.DELEVIRI.set()
     await message.reply("Доставка (полный адрес) / самовывоз")
 
 
 async def order_deleviri(message: types.Message, state: FSMContext):
-    # Получаем имя пользователя, сохраняем его и переходим к следующему состоянию
-    deleviri = message.text
-    await state.update_data(deleviri=deleviri)
+    # Получаем адрес доставки пользователя, сохраняем его и переходим к следующему состоянию
+    await state.update_data(deleviri=message.text)
     await OrderState.INFO.set()
     await message.reply("Напишите пожалуйста дополнительную информацию (дата, вес, декорации)")
 
 
 async def order_info(message: types.Message, state: FSMContext):
-    # Получаем адрес доставки пользователя, сохраняем его и переходим к подтверждению заказа
-    info = message.text
-    await state.update_data(info=info)
-    chat_id_nikita = 6622326689
+    # Получаем дополнительную информацию от пользователя и переходим к подтверждению заказа
+    await state.update_data(info=message.text)
     await OrderState.CONFIRMATION.set()
 
-    # Получаем все данные заказа
+    # Показываем данные заказа и спрашиваем, подтверждает ли он его
     data = await state.get_data()
-    insta = data.get('insta')
-    deleviri = data.get('deleviri')
-    info = data.get('info')
-
-    # Отправляем пользователю сведения о заказе и спрашиваем, подтверждает ли он его
-    confirmation_message = f"instagram: {insta}\nДоставка: {deleviri}\nДополнительно: {info}\n\nПодтверждаете заказ?"
+    confirmation_message = f"instagram: {data['insta']}\nДоставка: {data['deleviri']}\nДополнительно: {data['info']}\n\nПодтверждаете заказ?"
     await message.reply(confirmation_message,
                         reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, keyboard=[
                             [types.KeyboardButton('Да'), types.KeyboardButton('Нет')]
                         ]))
 
+
 async def order_finish(message: types.Message, state: FSMContext):
     # Получаем ответ пользователя и обрабатываем его
     if message.text == 'Да':
+        data = await state.get_data()
         # Если заказ подтвержден, отправляем сообщение об успешном оформлении заказа
         await message.reply("Ваш заказ успешно оформлен!")
+        text = f" Новый заказ:\n\ninstagram: {data['insta']}\n\nДоставка: {data['deleviri']}\n\nДоп. инф: {data['info']}"
+        await bot.send_message(6622326689, text)
     else:
         # Если заказ не подтвержден, отправляем сообщение о том, что заказ отменен
         await message.reply("Ваш заказ отменен.")
 
     # Сбрасываем состояние
     await state.finish()
-
-    text = f" Новый заказ:\n\ninstagram: {insta}\n\nДоставка: {deleviri}\n\nДоп. инф: {info}"
-    await bot.send_message(chat_id_nikita, text)
-
-    # Очищаем состояние FSM
-    await state.finish()
-
-    # Отправляем пользователю подтверждение о успешном оформлении заказа
-    await message.answer("Ваш заказ успешно оформлен.")
 
 
 def register_handler_client(dp: Dispatcher):
@@ -89,5 +71,5 @@ def register_handler_client(dp: Dispatcher):
     dp.register_message_handler(order_insta, state=OrderState.INSTA)
     dp.register_message_handler(order_deleviri, state=OrderState.DELEVIRI)
     dp.register_message_handler(order_info, state=OrderState.INFO)
-    dp.register_message_handler(lambda message: message.text in ['Да', 'Нет'], state=OrderState.CONFIRMATION)
+    dp.register_message_handler(order_finish, state=OrderState.CONFIRMATION)
     dp.storage = storage
